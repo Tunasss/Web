38a0da6b9748b94a9b6b854f243f4be7ae561eb3
Trần Anh Tuấn <83346864+Tunasss@users.noreply.github.com>
2021-11-19 20:35:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
 function move(elm) {
     elm.style.possiton = 'absolute'
     elm.style.top = Math.floor(Math.random() * 90 + 5) + '%'
     elm.style.left = Math.floor(Math.random()* 90 + 5 ) + '%'
 }
-
 var btn = document.querySelector('#no');
 btn.addEventListener('mouseover', function(e) { 
     move(e.target)
@@ -12,6 +10,5 @@ btn.addEventListener('mouseover', function(e) {
 
 var btn = document.querySelector('#yes');
 btn.addEventListener('click', function() {
-    alert("Yes babe ❤, I love you too")
+    alert("Dù học với thầy cô vài tháng, nhưng chúng em đã học rất vui, dù có ngày lớp quậy, lớp sôi nổi, lớp trầm, nhưng dù chỉ học qua màn ảnh nhỏ, bọn em vẫn rất yêu quý thầy cô, chúc thầy cô luôn mạnh khỏe, để có thể dìu dắt chúng em hết 3 năm cấp 3 này ạ, from 10 Tin with love <3")
 })
-
